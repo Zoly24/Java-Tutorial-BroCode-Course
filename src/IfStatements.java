7b75814eb460f6d0d2e8bc9ca309b99cc57746b3
Zoly24 <alexzanderwilburn2424@outlook.com>
2024-09-20 00:21:51 -0500

    Revamped IfStatements

--- src/IfStatements.java
+++ src/IfStatements.java
@@ -8,11 +8,23 @@ public class IfStatements {
         System.out.println("How old are you?"); //Questions the user about their age
         int age = detector.nextInt(); //Allows user to enter an age, storing it into the age variable
 
-        if(age >= 18) { //Read as "if the variable age they put in is greater than 18, then do what is below
-            System.out.println("You can now legally be tried as an adult in the US!");
+        if(age >= 79) { //Read as "if the variable age they put in is greater than 79, then do what is below"
+            System.out.println("You are a Silent Generation!");
         }
-        else{ //Read as "else if the variable age they put in is less than 18, then do what is below.
-            System.out.println("You can only be tried at a juvenile court in the US!");
+        else if(age >= 60){ //Read as "else if the variable age they put in is greater than 60, then do what is below".
+            System.out.println("You are a Baby Boomer!");
+        }
+        else if(age >= 44) { //Read as "else if the variable age they put in is greater than 44, then do what is below".
+            System.out.println("You are Generation X!");
+        }
+        else if(age >= 28) { //Read as "else if the variable age they put in is greater than 28, then do what is below".
+            System.out.println("You are a Millennial!");
+        }
+        else if(age >= 12) { //Read as "else if the variable age they put in is greater than 12, then do what is below".
+            System.out.println("You are Generation Z!");
+        }
+        else { //Read as "if all else doesn't apply, then do what is below".
+            System.out.println("You are Generation Alpha!");
         }
     }
 }
